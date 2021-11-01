04858b11a97d6f628bc27f72a848d5ceda6151c5
Sergey.Fokin <sergey.fokin@arcadia.spb.ru>
2021-11-01 16:12:37 +0300

    Add sample properties

---
+++ CGraphsApp/Edge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGraphsApp
+{
+    public class Edge
+    {
+        private float Weight { get; set; }
+    }
+}
---
+++ CGraphsApp/Vertex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGraphsApp
+{
+    public class Vertex
+    {
+        private List<Edge> Edges { get; set; }
+        public bool IsIsolated => Edges?.Count == 0;
+
+    }
+}
